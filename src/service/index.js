a7a80b1e1c5241b1bb542de338503e34027bae5d
[SW4/1반]신성민 <seongm22n@gmail.com>
2023-04-22 13:26:37 +0000

    Update index.js

--- src/service/index.js
+++ src/service/index.js
@@ -1,8 +1,11 @@
-// index.js
-
 const userService = require("./userService");
+const productsService = require("./productsService");
+const orderService = require("./orderService");
+
 
 module.exports = {
     userService,
+    productsService,
+    orderService
 };
 
