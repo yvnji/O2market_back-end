a2f8ffe4d4104ff2861955c9fb5794b86c26437c
[SW4/1반]신성민 <seongm22n@gmail.com>
2023-04-22 13:21:27 +0000

    Update index.js

--- src/data-access/schemas/index.js
+++ src/data-access/schemas/index.js
@@ -1,6 +1,9 @@
 const userSchema = require("./user");
-
+const productsSchema = require("./products");
+const orderSchema = require("./order");
 module.exports = {
     userSchema,
+    productsSchema,
+    orderSchema
 };
 
