65a8cbed2f7bcba5ff71669f1e72f9a14d49880d
[SW4/1반]신성민 <seongm22n@gmail.com>
2023-04-22 13:19:48 +0000

    Update index.js

--- src/data-access/index.js
+++ src/data-access/index.js
@@ -1,5 +1,9 @@
 const userDAO = require("./userDAO");
+const productsDAO = require("./productsDAO");
+const orderDAO = require("./orderDAO");
 
 module.exports = {
     userDAO,
+    productsDAO,
+    orderDAO
 };
