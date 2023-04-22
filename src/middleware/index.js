55c98cc069a9a7feed66df55a4d6f032e3ca5d5a
[SW4/1반]신성민 <seongm22n@gmail.com>
2023-04-22 13:24:30 +0000

    Update index.js

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,10 @@
 const userMiddleware = require("./userMiddleware");
+const productsMiddleware = require("./productsMiddleware");
+const orderMiddleware = require("./orderMiddleware");
+
 
 module.exports = {
     userMiddleware,
+    productsMiddleware,
+    orderMiddleware
 };
