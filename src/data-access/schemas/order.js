c5ff5208e5af57fefcd861f14bc6f6df626cb9cc
subin <subin012358@gmail.com>
2023-04-25 10:19:11 +0900

    order 수정, 삭제 완료

--- src/data-access/schemas/order.js
+++ src/data-access/schemas/order.js
@@ -1,14 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = require('mongoose');
-// const Types = require("mongoose");
 const orderItemsSchema = require('./orderItems');
 
 const orderSchema = new mongoose.Schema(
   {
-    _id: {
-      type: Schema.Types.ObjectId,
-      required: true,
-    },
     userId: { type: Schema.Types.ObjectId, ref: 'users', required: true },
     orderItems: [orderItemsSchema],
     orderAddr: {
